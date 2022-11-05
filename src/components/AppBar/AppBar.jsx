c6c4254a9c245129962d9ab66a1de6b5142af4ed
Volodymyr Labunets <vplabunets@gmail.com>
2022-11-05 12:29:24 +0200

    xxx4

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -4,10 +4,10 @@ import { NavItem } from './AppBar.styled';
 const AppBar = () => {
   return (
     <Header>
-      <NavItem to="/" end>
+      <NavItem to="/goit-react-hw-05-movies_2/" end>
         Home
       </NavItem>
-      <NavItem to="/movies">Movies</NavItem>
+      <NavItem to="/goit-react-hw-05-movies_2/movies">Movies</NavItem>
     </Header>
   );
 };
