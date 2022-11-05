8f908b2ab01976c97cdd24e7b0131ad0b0dd837b
Volodymyr Labunets <vplabunets@gmail.com>
2022-11-05 12:30:59 +0200

    xxx6

--- src/Pages/Movies/Movies.jsx
+++ src/Pages/Movies/Movies.jsx
@@ -52,7 +52,7 @@ export const Movies = () => {
             <li key={film.id}>
               <Link
                 id={film.id}
-                to={`/movies/${film.id}`}
+                to={`/goit-react-hw-05-movies_2/movies/${film.id}`}
                 state={{ from: location }}
               >
                 {film.title}{' '}
