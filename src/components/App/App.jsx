468c24f6485cee60118f12935805954c4fa593c4
Volodymyr Labunets <vplabunets@gmail.com>
2022-11-07 23:37:29 +0200

    fixing suspense

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,5 @@
 import { lazy } from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import Layout from '../UI/Layout/Layout';
 import AppWrap from './App.styled';
 
@@ -22,6 +22,7 @@ export const App = () => {
             <Route path="cast" element={<Cast />} />
             <Route path="reviews" element={<Reviews />} />
           </Route>
+          <Route path="*" element={<Navigate to="/" />} />
         </Route>
       </Routes>
     </AppWrap>
--- src/components/UI/Layout/Layout.jsx
+++ src/components/UI/Layout/Layout.jsx
@@ -2,12 +2,31 @@ import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 import AppBar from 'components/AppBar/AppBar';
 import LayoutBox from './Layout.styled';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Layout = () => {
   return (
     <LayoutBox>
       <AppBar />
-      <Suspense fallback={<div>Loading...</div>}>
+      <Suspense
+        fallback={
+          <div>
+            <ToastContainer
+              position="top-right"
+              autoClose={3000}
+              hideProgressBar={false}
+              newestOnTop={false}
+              closeOnClick
+              rtl={false}
+              pauseOnFocusLoss
+              draggable
+              pauseOnHover
+              theme="light"
+            />
+          </div>
+        }
+      >
         <Outlet />
       </Suspense>
     </LayoutBox>
